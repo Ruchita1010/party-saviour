4e3caf491b6a0449336eb693d6adf8381611df4b
Ruchita Gosavi <ruchigosavi10@gmail.com>
2023-07-26 23:51:18 +0530

    feat: scale the plane and put the args in Rigidbody

--- src/components/Backyard.jsx
+++ src/components/Backyard.jsx
@@ -7,11 +7,12 @@ export const Backyard = () => {
       <ambientLight intensity={0.8} />
       <OrbitControls />
       {/* Ground */}
-      <RigidBody colliders={false} type="fixed">
-        <mesh
-          scale={[10, 10, 10]}
-          position={[0, -0.2, 0]}
-          rotation={[-Math.PI / 2, 0, 0]}>
+      <RigidBody
+        type="fixed"
+        scale={[30, 30, 30]}
+        position={[0, 0, -120]}
+        rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh>
           <planeGeometry args={[10, 10]} />
           <meshStandardMaterial color={0x136d15} />
         </mesh>
